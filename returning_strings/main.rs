206f03f70b69dbcafc0d045e647d321e794dd63a
Tatu Pössi <42032491+Takkakynttila@users.noreply.github.com>
2022-05-22 15:36:35 +0300

    Create main.rs

---
+++ returning_strings/main.rs
@@ -0,0 +1,12 @@
+//Make a function that will return a greeting statement that uses an input; your program should return,
+//"Hello, <name> how are you doing today?".
+
+//[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+fn main() {
+    fn greet(name: &str) -> String {
+        return format!("Hello, {} how are you doing today?", name);
+    }
+    let x = greet("Tatu");
+    println!("{}", x);
+}
